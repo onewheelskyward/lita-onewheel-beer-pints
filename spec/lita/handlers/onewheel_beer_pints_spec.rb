4dc61886228eb1789ecc8096e13e80ed82f4e87f
Andrew Kreps <andrew.kreps@gmail.com>
2016-07-05 22:34:24 -0700

    Beer base fix for #'d taps.

--- spec/lita/handlers/onewheel_beer_pints_spec.rb
+++ spec/lita/handlers/onewheel_beer_pints_spec.rb
@@ -20,9 +20,14 @@ describe Lita::Handlers::OnewheelBeerPints, lita_handler: true do
     expect(replies.last).to eq("Pints taps: 1) Brick House Blonde 2) Seismic IPA 3) Rip Saw Red 4) Steel Bridge Stout 5) You’re A Peach, Hon’………5.9% ABV – 18 IBU 6) Belgian Breakfast Beer 7) Chocolate Blood Orange Candi Biere 8) Pints Single Hop Pale Series………5.4% ABV – 45 IBU 9) Konvention Kölsch………")
   end
 
-  it 'displays details for tap 4' do
+  it 'displays details for tap brick' do
     send_command 'pints brick'
     expect(replies.last).to eq("Pints's tap 1) Brick House Blonde 5.0% ABV 18 IBU - She’s blonde and refreshing! She’s mighty mighty! Brewed with perfect proportions of Northwest hops and malts for a beer that makes an old man wish for younger days. This session ale lets it all hang out with easy drinkability and a light malt finish. …what a winning hand!, ")
   end
 
+  it 'displays details for tap 7' do
+    send_command 'pints 7'
+    puts replies.last
+    expect(replies.last).to include("Pints's tap 7) Chocolate Blood Orange Candi Biere 5.9% ABV 24 IBU - This brew is inspired by the")
+  end
 end
